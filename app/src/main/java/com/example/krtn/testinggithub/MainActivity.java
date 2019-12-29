2876211e3d78f24f5ebbe7e24a3f779424b8fb2b
kirtanparmar <kirtanhq@gmail.com>
2019-12-29 11:09:19 +0530

    Initial commit

---
+++ app/src/main/java/com/example/krtn/testinggithub/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.krtn.testinggithub;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        tv = findViewById(R.id.tv);
+        tv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Hello world", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
