0d24931f17cdc6f098e63db0ea1d0c59a8601aa7
kirtanparmar <kirtanhq@gmail.com>
2019-12-29 12:25:28 +0530

    Committing Branch01 for checking difference with main branch.

--- app/src/main/java/com/example/krtn/testinggithub/MainActivity.java
+++ app/src/main/java/com/example/krtn/testinggithub/MainActivity.java
@@ -9,7 +9,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    TextView tv;
+    TextView tv, tv1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         tv = findViewById(R.id.tv);
+        tv1 = findViewById(R.id.tv1);
         tv.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(MainActivity.this, "Master Branch in GitHub", Toast.LENGTH_SHORT).show();
                 tv.setText("This is Master Branch Changed by Branch01");
+            }
+        });
+
+        tv1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Checking difference in GitHub", Toast.LENGTH_SHORT).show();
             }
         });
     }
