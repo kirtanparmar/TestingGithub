8c61644fc57f06d91df26ab3d599b66b378e7bdb
kirtanparmar <kirtanhq@gmail.com>
2020-01-06 12:13:28 +0530

    Committing Branch01 for checking difference with main branch.

--- app/src/main/java/com/example/krtn/testinggithub/Git.java
+++ app/src/main/java/com/example/krtn/testinggithub/Git.java
@@ -2,7 +2,7 @@ package com.example.krtn.testinggithub;
 
 public class Git {
   
-  int gitVirsion = 0;
-  String Branch02 = "This is Branch02.";
+    int gitVirsion = 0;
+    String Branch02 = "This is Branch02.";
 
 }
